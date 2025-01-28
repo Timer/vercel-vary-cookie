f9090a18688f8c31d2b6da2e42d73c38cb60e382
Joe Haddad <timer@vercel.com>
2025-01-27 23:47:52 -0500

    hmm 3

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ export default function handler(req, res) {
     const hasCookie = req.headers.cookie;
 
     if (hasCookie) {
-        res.status(200).json({ message: `Hello with cookie: ${JSON.stringify(req.cookies)}` });
+        res.status(200).json({ message: `Hello with cookie: ${JSON.stringify(req.headers.cookie)}` });
     } else {
         res.setHeader('Cache-Control', 's-maxage=10, stale-while-revalidate');
         res.status(200).json({ message: 'Hello cached' });
